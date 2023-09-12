1ada81716e033afabc826386e9e514d59feacc6a
2100031335 <109579699+2100031335@users.noreply.github.com>
2023-09-12 21:31:38 +0530

    Delete Bharath Intern directory

--- Bharath Intern/script.js
+++
@@ -1,9 +0,0 @@
-const celsiusInput = document.getElementById('celsius');
-const fahrenheitInput = document.getElementById('fahrenheit');
-const convertBtn = document.getElementById('convertBtn');
-
-convertBtn.addEventListener('click', () => {
-    const celsiusValue = parseFloat(celsiusInput.value);
-    const fahrenheitValue = celsiusValue * 9 / 5 + 32;
-    fahrenheitInput.value = fahrenheitValue.toFixed(2);
-});
